64bdfaba7936c0cbe0a59131dce0f94c778766dc
Amauri Brandão <amaurinogueira@gmail.com>
2021-10-13 11:35:05 -0400

    alteracao_na_senha

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :usuarios, :path_names => {
+    :sing_in => 'entrar',
+    :sing_out => 'sair',
+    :sing_up => 'cadastrar',
+    :password => 'senha'
+  }
+
   namespace :dashboard do
     resources :home, :path => '/'
   end  
+
+  namespace :saude, :path => '/' do 
+    resources :home, :path => '/'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
